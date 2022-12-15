b4629ad35b6850c7d5df0f60d977cfcc21dfd70e
198w1f0010 <106279299+198w1f0010@users.noreply.github.com>
2022-12-15 17:38:09 +0530

    code

---
+++ 403-event-listeners/AdvanceJS-403-02/script.js
@@ -0,0 +1,39 @@
+function click_w(){
+   var trum=new Audio('./selftimer 2sec.mp3')
+   trum.play();
+}
+
+function click_a(){
+    var boss=new Audio('./small.mp3')
+    boss.play();
+ }
+
+ function click_s(){
+    var boss=new Audio('./love-hearts.mp3')
+    boss.play();
+ }
+
+ function click_d(){
+    var boss=new Audio('./samsung-galaxy.mp3')
+    boss.play();
+ }
+
+ function click_j(){
+    var boss=new Audio('./hi.mp3')
+    boss.play();
+ }
+
+ function click_k(){
+    var boss=new Audio('./never.mp3')
+    boss.play();
+ }
+
+ function click_l(){
+    var boss=new Audio('./oye.mp3')
+    boss.play();
+ }
+ 
+ 
+ 
+
+ 
